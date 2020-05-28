682a45f1f2efa8e53b3cc8a0832a11b3a055d806
Yasmin Knowles <65784660+yazzie001@users.noreply.github.com>
2020-05-28 21:19:25 +0100

    Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions which are required for the Week 3 R Programming assignment
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates an R object that stores a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list(set = set,
+	get = get,
+	setinverse = setinverse,
+	getinverse = getinverse
+	)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve requires an argument that is returned by makeCacheMatrix.
+## It retrieves the inverse from the cached matrix that is stored in the 
+##makeCacheMatrix object's environemnt
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+i <- x$getinverse()
+if (!is.null(i)) {
+	message("getting cached data")
+	return(i)
+}
+data <- x$get()
+i <- solve(data, ...)
+x$setinverse(i)
+i
 }
